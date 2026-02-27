f4c3b5db5390f22cc736da897e566094745e1ed9
Yuta-N-18 <r202304726py@jindai.jp>
2026-02-27 14:55:24 +0900

    バリデーション処理時のエラーメッセージの表示

--- example-app/resources/views/task-edit.blade.php
+++ example-app/resources/views/task-edit.blade.php
@@ -18,8 +18,11 @@
                 :value="old('title', $task_title)"
                 class="w-full"
             />
+            <x-input-error :messages="$errors->get('title')"/>
+
             <x-input-label for="comment" :value="'コメント'"/>
             <x-textarea name="comment" id="comment" rows=12 class="w-full">{{ old('comment', $task_comment) }}</x-textarea>
+            <x-input-error :messages="$errors->get('comment')"/>
 
             <div class="flex justify-end gap-4">
                 <x-secondary-button :href="route('task.showall')">
